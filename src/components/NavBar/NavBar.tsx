e6007002e690333183ff222666c113bfc257620f
Susie Kim <35243112+susiekim101@users.noreply.github.com>
2025-12-28 23:15:37 -0800

    feat: set up react routing

---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,19 @@
+const NavBar = () => {
+
+    return (
+        <div>
+            <div>susiekim.dev</div>
+
+            <div>
+                <p>About</p>
+                <p>Projects</p>
+                <p>Experience</p>
+                <p>Blog</p>
+            </div>
+
+            <div/>
+        </div>
+    );
+}
+
+export default NavBar;
